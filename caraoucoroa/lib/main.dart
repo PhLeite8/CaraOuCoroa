7fdb0d8d866ce8d656f0dcb6e0bb34ba5c06dc2d
Pedro <pedrowwyx@gmail.com>
2023-05-04 22:24:52 -0300

    App Cara ou Coroa v1

---
+++ caraoucoroa/lib/main.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api
+
+import 'Views/jogar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: CaraCoroa(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+
